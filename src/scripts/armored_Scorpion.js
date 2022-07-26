c10cb80f48c6f076555195fb77a05d888d8b7acb
miker704 <miker704@gmail.com>
2022-07-26 14:22:59 -0400

    added props

--- src/scripts/armored_Scorpion.js
+++ src/scripts/armored_Scorpion.js
@@ -14,7 +14,20 @@ class ArmoredScorpion extends MovingObject {
                 color: Util.randomColors(),
                 game: options.game
             }
-        )
+        );
+
+         //special properties for spawn of scorpion they are
+        //2 versions the classic version that moves horizontally 
+        // and another exclusive to this game that tracks the player 
+        // and has armor
+
+        this.armor = 10;
+        this.health = 10;
+
+        this.direction = options.direction;
+        this.maxVelocity = options.maxVelocity;
+        this.mushroomTobePoisoned = null;
+
     }
 }
 
