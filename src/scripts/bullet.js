8c297d998fb396e2b13955a9cdfb75a0d5a3a307
miker704 <miker704@gmail.com>
2022-07-26 20:19:01 -0400

    added bullet detection to new enemies

--- src/scripts/bullet.js
+++ src/scripts/bullet.js
@@ -5,7 +5,10 @@ import Mushrooms from "./mushrooms.js";
 import Centipede from "./centipede.js";
 import Flea from "./flea.js";
 import Scorpion from "./scorpion.js";
-
+import ArmoredScorpion from "./armored_Scorpion.js";
+import JumpingSpider from "./jumping_Spider.js";
+import Wasp from "./wasp.js";
+import LightningWasp from "./lightning_Wasp.js";
 
 class Bullet extends MovingObject {
     constructor(options) {
@@ -32,8 +35,13 @@ class Bullet extends MovingObject {
             entity instanceof Spider ||
             entity instanceof Centipede ||
             entity instanceof Scorpion ||
-            entity instanceof Flea) {
-       
+            entity instanceof Flea ||
+            entity instanceof ArmoredScorpion ||
+            entity instanceof Wasp ||
+            entity instanceof JumpingSpider ||
+            entity instanceof LightningWasp
+            ) {
+
             entity.hitByZapper(); // call to remove entity
             this.game.removeEntity(this); // remove the bullet from the game 
         }
