6887ac740ea5763db9678d7758989e80a9ddf4f0
miker704 <miker704@gmail.com>
2022-07-28 15:36:32 -0400

    added damage feature

--- src/scripts/mushrooms.js
+++ src/scripts/mushrooms.js
@@ -2,6 +2,7 @@
 import BugZapper from "./bugZapper.js";
 import Bullet from "./bullet.js";
 import MovingObject from "./movingObject.js"
+import PowerUps from "./powerUps.js";
 import Util from "./utils.js";
 
 
@@ -25,8 +26,9 @@ class Mushrooms extends MovingObject {
 
         // this.health = this.game.mushroomHealth();
         // this.startingHealth = this.health;
-        // this.damage = [];
-        this.health = 2;
+        this.damage = [];
+        this.health = 4;
+        this.poisoned = false;
     }
 
 
@@ -44,16 +46,15 @@ class Mushrooms extends MovingObject {
 
 
     draw(ctx) {
-
         //poison colors
         var mgrd = ctx.createRadialGradient(this.pos[0] + 1, this.pos[1] + 1, this.radius / 2, this.pos[0], this.pos[1], this.radius);
         mgrd.addColorStop(0, "#9D00FF");
         //#9063CD   #C724B1    #9D00FF
         mgrd.addColorStop(0.6, "#2E1A47");
-        
+
+
 
         ctx.fillStyle = this.isPoisoned() ? mgrd : this.color;
-
         //create the the mushroom stem
         let mushroomStem = 8;
         ctx.fillRect(this.pos[0] - mushroomStem / 2, this.pos[1] - this.radius / 2, mushroomStem, this.radius * 1.5);
@@ -65,42 +66,65 @@ class Mushrooms extends MovingObject {
         // ctx.stroke();
 
         // check if the mushroom is at full health
-        // if (this.health === this.startingHealth) {
-        //     return;
-        // }
+        if (this.health === 4) {
+            return;
+        }
 
-        //     ctx.fillStyle = '#000000';
-        //     let wound = (4-(this.startingHealth / 2));
-        //     for (let i = 0; i < this.startingHealth - this.health; i++) {
+        ctx.fillStyle = '#000000';
+        let wound = (6 - (4 / 2));
+        for (let i = 0; i < 4 - this.health; i++) {
 
-        //         while (this.damage.length < i + 1) {
-        //             this.damage.push(Math.random() * this.radius * 1.5 + this.pos[0] - this.radius * (3 / 4));
-        //         }
+            while (this.damage.length < i + 1) {
+                this.damage.push(Math.random() * this.radius * 1.5 + this.pos[0] - this.radius * (3 / 4));
+            }
 
-        //                     ctx.beginPath();
-        //                     ctx.arc(this.damage[i],this.pos[1]-this.radius*(wound/10) ,wound,0,2*Math.PI,false);
-        //                     ctx.fill();
-        //     }
+            ctx.beginPath();
+            ctx.arc(this.damage[i], this.pos[1] - this.radius * (0.90-0.0555555*(wound)), wound, 0, 2 * Math.PI, false);
+            ctx.fill();
+        }
 
 
 
     }
 
     hitByZapper() {
-        this.health--;
-        // console.log("hitting mushroom:"+ this.health );
+
+
+
+        
+        this.game.addSparks(
+            {
+                pos: this.pos.slice(),
+                amount: 8,
+                color: this.color
+            }
+        );
 
         this.game.incrementScore(10);
+
+
         if (this.health === 0) {
+
+           if(Math.random() > 0.9){
+            this.game.addPowerUp(this.pos.slice());
+           }
+
+
             this.game.removeEntity(this);
 
-            // console.log("destroyed mushroom");
-            this.game.incrementScore(50);
-            // this.game.removeEntity(this);
-            return;
+            if (this.poisoned === true) {
+                this.game.incrementScore(100);
+            }
+            else {
+                this.game.incrementScore(50);
+            }
+            
 
         }
-
+        else {
+            this.health--;
+        }
+        this.color = Util.randomColors();
     }
 
 
@@ -113,9 +137,7 @@ class Mushrooms extends MovingObject {
             console.log("currently collat with mushrooms");
 
         }
-        // if(entity instanceof Bullet){
-        //     this.hitByZapper();
-        // }
+
     }
 
 }
