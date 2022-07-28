d498678f24fb92dd20a6f3dff394305b853bfb0d
miker704 <miker704@gmail.com>
2022-07-28 15:36:01 -0400

    final version

--- src/scripts/jumping_Spider.js
+++ src/scripts/jumping_Spider.js
@@ -8,11 +8,13 @@ class JumpingSpider extends MovingObject {
         super({
             pos: options.pos,
             radius: 21,
-            vel: [0, 0],
+            vel: options.vel,
             color: Util.randomColors(),
             game: options.game
         });
         // console.log(this.radius)
+        this.armor = 1;
+        this.health = 3;
 
         this.direction = options.direction;
         this.maxVelocity = options.maxVelocity;
@@ -26,14 +28,15 @@ class JumpingSpider extends MovingObject {
     draw(ctx) {
         // Draw the background
         // Draw the spider
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = Util.randomColors();
         ctx.beginPath();
-        ctx.arc(this.pos[0], this.pos[1] - 8, 8, 0, 2 * Math.PI);
+        ctx.arc(this.pos[0], this.pos[1] - 8, 10, 0, 2 * Math.PI);
         ctx.arc(this.pos[0], this.pos[1] + 8, 8, 0, 2 * Math.PI);
         ctx.fill();
         ctx.beginPath();
         // Left Legs
         ctx.fillStyle = this.color;
+        ctx.strokeStyle = "gold";
         ctx.moveTo(this.pos[0], this.pos[1]);
         ctx.lineTo(this.pos[0] - 30, this.pos[1] - 30);
         ctx.lineTo(this.pos[0] - 30, this.pos[1] - 35);
@@ -64,13 +67,15 @@ class JumpingSpider extends MovingObject {
     }
 
     move() {
+        this.waspBounciness();
+        // this.rotation();
+        // this.increaseVelocity();
         this.pos[0] += this.vel[0];
         this.pos[1] += this.vel[1];
-        this.rotation();
-        this.increaseVelocity();
+
     }
 
-    currentDistanceFromPlayer(){
+    currentDistanceFromPlayer() {
         let xCoord = this.pos[0] - this.game.zapper.pos[0];
         let yCoord = this.pos[1] - this.game.zapper.pos[1];
         //using the distance formula
@@ -81,23 +86,48 @@ class JumpingSpider extends MovingObject {
 
 
     hitByZapper() {
-        // console.log("removed spider");
+
+        this.game.addSparks(
+            {
+                pos: this.pos.slice(),
+                amount: 21,
+                color: this.color
+            }
+        );
+
+
         let distanceForScore = this.currentDistanceFromPlayer();
         let rangeScore = 0;
-        if(distanceForScore <=100){
+        if (distanceForScore <= 100) {
             rangeScore = 900;
         }
-        else if( distanceForScore > 100 && distanceForScore <= 200){
+        else if (distanceForScore > 100 && distanceForScore <= 200) {
             rangeScore = 600;
         }
-        else{
+        else {
             rangeScore = 300;
         }
 
 
+        if (this.armor !== 0) {
+            this.armor--;
+        }
+        else {
+            this.health--;
+            this.vel = this.health === 3 && this.armor === 0 ?
+                [this.vel[0] + 0.5, this.vel[1] + 0.5] :
+                this.health === 2 ?
+                    [this.vel[0] + 1.25, this.vel[1] + 1.25] :
+                    [this.vel[0] + 1.5, this.vel[1] + 1.5] ;
+            //[this.vel[0] +(this.vel[0] * 0.5), this.vel[1]+(this.vel[1] * 0.5)];
 
-        this.game.removeEntity(this);
-        this.game.incrementScore(rangeScore);
+        }
+        if (this.health === 0 && this.armor === 0) {
+
+            this.game.removeEntity(this);
+            this.game.incrementScore(rangeScore);
+        }
+
 
     }
 
@@ -114,15 +144,15 @@ class JumpingSpider extends MovingObject {
     }
 
 
-    increaseVelocity(){
+    increaseVelocity() {
         let velocityofX = this.maxVelocity * Math.cos(this.direction);
-        let velocityofY = this.maxVelocity *   Math.sin(this.direction);
-        this.vel = [velocityofX,velocityofY];
+        let velocityofY = this.maxVelocity * Math.sin(this.direction);
+        this.vel = [velocityofX, velocityofY];
     }
 
-    rotation(){
-        let rotationAmount = Math.min(Math.abs(this.findPlayer()-this.direction),Math.PI/8);
-        this.direction+= this.findPlayer()> this.direction ? rotationAmount : (rotationAmount*-1);
+    rotation() {
+        let rotationAmount = Math.min(Math.abs(this.findPlayer() - this.direction), Math.PI / 8);
+        this.direction += this.findPlayer() > this.direction ? rotationAmount : (rotationAmount * -1);
     }
 
 
