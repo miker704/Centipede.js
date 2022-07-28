a715e954b9aed11372615d1aff716a9324449f66
miker704 <miker704@gmail.com>
2022-07-28 15:35:12 -0400

    final version added new props

--- src/scripts/bugZapper.js
+++ src/scripts/bugZapper.js
@@ -8,9 +8,11 @@ import Centipede from "./centipede.js";
 import Flea from "./flea.js";
 import ZapperFire from "./zapperFire.js";
 import Scorpion from "./scorpion.js";
-import JumpingSpider from "./jumping_Spider.js";
+import ArmoredScorpion from "./armored_Scorpion.js";
 import Wasp from "./wasp.js";
+import JumpingSpider from "./jumping_Spider.js";
 import LightningWasp from "./lightning_Wasp.js";
+import PowerUps from "./powerUps.js";
 
 // the player is the bugzapper that can be controlled
 //player can move the zapper range of up to 30% of the bottom
@@ -18,8 +20,8 @@ import LightningWasp from "./lightning_Wasp.js";
 
 class BugZapper extends MovingObject {
 
-    constructor (options) {
-        
+    constructor(options) {
+
 
         super({
             pos: options.pos,
@@ -36,20 +38,23 @@ class BugZapper extends MovingObject {
             game: this.game,
             bugZapper: this
         });
-
+        this.hasSplayShot = false;
+        this.hasShield = false;
     }
 
-    fireZap () {
+    fireZap() {
+
         this.game.addEntities(new Bullet({
             game: this.game,
             vel: this.vel,
-            pos: this.pos
-
+            pos: this.pos.slice()
         }));
+
+
     }
 
-    // undecided on what the bugzapper will be triangle delta arrow
-    draw (ctx) {
+
+    draw(ctx) {
         let tempRadius = (this.radius / 3) * 5;
         let x = 20;
         let y = 20;
@@ -63,33 +68,26 @@ class BugZapper extends MovingObject {
         ctx.lineTo(this.pos[0] - tempRadius * Math.cos(7 * Math.PI / 6), this.pos[1] - tempRadius * Math.sin(7 * Math.PI / 6));
         ctx.lineTo(this.pos[0] - tempRadius * Math.cos(-1 * Math.PI / 6), this.pos[1] - tempRadius * Math.sin(-1 * Math.PI / 6));
 
-
         ctx.closePath();
-
-
-
-        // ctx.lineTo(
-        //     x + this.radius * Math.cos(this.dir - (Math.PI / 2)),
-        //     y + this.radius * Math.sin(this.dir - (Math.PI / 2))
-        //   );
-        //   ctx.lineTo(
-        //     x + this.radius * Math.cos(this.dir + (5 * Math.PI / 6)),
-        //     y + this.radius * Math.sin(this.dir + (5 * Math.PI / 6))
-        //   );
-        //   ctx.closePath();
-
-
-
-
 
         ctx.fillStyle = this.color;
         ctx.fill();
         ctx.stroke();
 
+        //player loses life or gets shield power up give them shield to prevent 
+        //losing a lives too fast for instant game over
+        if (this.hasShield === true) {
+            ctx.strokeStyle = Util.randomColors();
+            ctx.beginPath();
+            ctx.arc(this.pos[0], this.pos[1], tempRadius*2, 0, 2 * Math.PI, false);
+            ctx.stroke();
+        }
+
+
     }
 
 
-    power (impulse) {
+    power(impulse) {
         this.vel[0] += impulse[0];
         this.vel[1] += impulse[1];
         // console.table(this.vel);
@@ -105,7 +103,7 @@ class BugZapper extends MovingObject {
 
     }
 
-    slowZapper (distance) {
+    slowZapper(distance) {
         distance = distance || 0.02;
         for (let i = 0; i < 2; i++) {
             //check if velocity reached a negative number
@@ -123,7 +121,7 @@ class BugZapper extends MovingObject {
     // according to the orginal games code in assembly in seems that 
     // the movement per key press if using keys it moves the length
     //of the with of a mushroom
-    move () {
+    move() {
         this.pos[0] += this.vel[0];
         this.pos[1] += this.vel[1];
         //figured out that in moveobject class this function is overided
@@ -134,9 +132,9 @@ class BugZapper extends MovingObject {
         //bug zapper to be around 30-40 % of the bottom of the
         // game canvas
         // x axis prevent from moving outside the width of canvas
-        let twoThirdOfScreen = ((Game.DIM_Y - 36) *2/3) + 36;
-        let playPos = twoThirdOfScreen - (twoThirdOfScreen%24);
-        let topLimit= playPos + (24/2); // -> thhis will be use as the playerarea
+        let twoThirdOfScreen = ((Game.DIM_Y - 36) * 2 / 3) + 36;
+        let playPos = twoThirdOfScreen - (twoThirdOfScreen % 24);
+        let topLimit = playPos + (24 / 2); // -> thhis will be use as the playerarea
         let testRange = 36;
 
         // console.log("in bugzappper move");
@@ -162,8 +160,8 @@ class BugZapper extends MovingObject {
             this.pos[1] = Game.DIM_Y - this.radius;
             this.vel[1] = this.vel[1] < 0 ? this.vel[1] : 0;
         }
-        else if (this.pos[1] - this.radius < topLimit) {
-            this.pos[1] = topLimit + this.radius;
+        else if (this.pos[1] - this.radius < testRange) {
+            this.pos[1] = testRange + this.radius;
             this.vel[1] = this.vel[1] > 0 ? this.vel[1] : 0;
         }
 
@@ -174,20 +172,47 @@ class BugZapper extends MovingObject {
     // object of enemy type the player loses a life
     // if they collide with a mushroom they cannot move throw
     //it
-    collisonDetection (entity) {
+    collisonDetection(entity) {
 
-    
         if ((entity instanceof Spider || entity instanceof Centipede ||
             entity instanceof Flea || entity instanceof Scorpion ||
-            entity instanceof ArmoredScorpion || 
+            entity instanceof ArmoredScorpion ||
             entity instanceof Wasp ||
             entity instanceof JumpingSpider ||
             entity instanceof LightningWasp
-            )) {
+        ) && !this.hasShield) {
 
+            //player loses life activate shield and clear poison mushrooms
+
+            this.game.giveShield();
             this.game.lives--;
+            this.game.clearPoisonMushrooms();
+
+
+            if (this.game.lives > 0) {
+                // set timer for shield
+                setTimeout(function(){
+                    this.hasShield = false;
+
+                }.bind(this),8000);
+            }
+
+
+
+            //  this.game.AllMushrooms.forEach(
+            //     (mushroom) => {
+            //         mushroom.poisoned = false;
+            //     }, this);
 
         }
+
+        if ((entity instanceof PowerUps)) {
+
+            entity.collisonDetection(this);
+            this.game.removeEntity(entity);
+
+        }
+
 
     }
 
